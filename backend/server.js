b9a0ba37c2d51d9acccd2df52fd51339393b097d
rzayev558 <rzayev558@gmail.com>
2023-05-11 13:28:31 +0200

    mongoDB setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("server...");
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.get("/api/users", (req, res) => {
+  res.status(200).json({ message: "Welcome to the support" });
+});
+
+app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
