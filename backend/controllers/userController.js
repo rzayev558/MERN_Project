896d1eac670e592e22e3270379ab57304eb60a92
rzayev558 <rzayev558@gmail.com>
2023-05-13 12:29:33 +0200

    register user BE

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,17 +1,49 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+
+const User = require("../models/userModel");
 //desc Register a new user
 //@route /api/users
 //@access Public
 const registerUser = asyncHandler(async (req, res) => {
-  const { name, email, pwd } = req.body;
+  const { name, email, password } = req.body;
 
   //validation
-  if (!name || !email || !pwd) {
+  if (!name || !email || !password) {
     res.status(400);
     throw new Error("Include all fields");
   }
 
-  res.send("Register Router");
+  //Find if user already exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  //Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  //Create user
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new error("Invalid user data");
+  }
 });
 
 //desc login a new user
